ed667238e5d0ba519835eccf2f06909c02af3cfb
Guilherme <guizmo1992@hotmail.com>
2022-10-09 16:14:55 -0300

    css: created the dropdownmenu and minor changes into gallerieslist

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,67 @@
+import { useGalleries } from '../hooks/useGalleries'
+import styles from '../styles/DropdownMenu.module.scss'
+
+function DropdownMenu() {
+  const { changeCurrentSection, changeSort, changeWindow } = useGalleries()
+
+  return (
+    <>
+      <div className={styles.dropdown}>
+        <button type="button" className={styles.dropdown__btn}>
+          Section
+        </button>
+        <div className={styles.content}>
+          <button type="button" className={styles.content__btn} onClick={() => changeCurrentSection('hot')}>
+            Hot
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeCurrentSection('top')}>
+            Top
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeCurrentSection('user')}>
+            User
+          </button>
+        </div>
+      </div>
+      <div className={styles.dropdown}>
+        <button type="button" className={styles.dropdown__btn}>
+          Section
+        </button>
+        <div className={styles.content}>
+          <button type="button" className={styles.content__btn} onClick={() => changeSort('viral')}>
+            Hot
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeSort('top')}>
+            Top
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeSort('time')}>
+            Time
+          </button>
+        </div>
+      </div>
+      <div className={styles.dropdown}>
+        <button type="button" className={styles.dropdown__btn}>
+          Section
+        </button>
+        <div className={styles.content}>
+          <button type="button" className={styles.content__btn} onClick={() => changeWindow('Day')}>
+            Day
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeWindow('Week')}>
+            Week
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeWindow('Month')}>
+            Month
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeWindow('Year')}>
+            Year
+          </button>
+          <button type="button" className={styles.content__btn} onClick={() => changeWindow('All')}>
+            All
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DropdownMenu
