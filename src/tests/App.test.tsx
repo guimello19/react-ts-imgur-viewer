897ead37e54c29bfdaaf2edd14b0ab0f40b60cd3
Guilherme <guizmo1992@hotmail.com>
2022-10-09 22:12:27 -0300

    test: simples render validate

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import React from 'react'
 import App from '../App'
 import '@testing-library/jest-dom'
 
 describe('Imgur app', () => {
   test('render app component', () => {
     render(<App />)
+    const galleryList = screen.getByTestId('gallery-section')
+    expect(galleryList).toBeTruthy()
   })
 })
